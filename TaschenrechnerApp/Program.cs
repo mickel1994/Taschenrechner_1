d7826c496e3f31fb39fb1d0b490f39d1d8c68968
mickel1994 <michistich94@gmail.com>
2021-02-11 13:08:08 +0100

    Initial Ceck- in of Taschenrechner

---
+++ TaschenrechnerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaschenrechnerApp
+{
+    class MainClass
+    {
+         static void Main(string[] args)
+        {
+            //User Story "Addieren": Als Benutzer mcöhte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
+            Console.WriteLine("Bitte geben Sie den ersten Summanden ein:");
+            string ersterSummand = Console.ReadLine();
+            Console.WriteLine("Bitte geben Sie den zweiten Summanden ein");
+            string zweiterSummand = Console.ReadLine();
+
+            //Wandel Text in Ganzzahlen
+            int ersterSummandalsZahl = Convert.ToInt32(ersterSummand);
+            int zweiterSummandalsZahl = Convert.ToInt32(zweiterSummand);
+
+            //Berechnung ausführen
+            int Summe = ersterSummandalsZahl + zweiterSummandalsZahl;
+
+            //Ausgabe
+
+            Console.WriteLine("Die Summe ist: {0}", Summe);
+            Console.ReadLine();
+
+
+        }
+    }
+}
